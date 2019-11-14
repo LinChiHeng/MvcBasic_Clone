f1ca07aa871911f1b55638dc701b5e4e90e12ae2
Henry Lin <chlin97701021@hotmail.com>
2019-11-14 16:29:15 +0800

    MVC專案1114

---
+++ MvcBasic_Clone/Controllers/PassDataController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcBasic_Clone.Models;
+using MvcBasic_Clone.ViewModels;
+
+namespace MvcBasic_Clone.Controllers
+{
+    public class PassDataController : Controller
+    {
+        // GET: PassData
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PassViewData()
+        {
+            ViewData["Name"] = "Kevin";
+            ViewData["Age"] = 33;
+            ViewData["Single"] = true;
+
+            return View();
+        }
+
+        public ActionResult PassViewBag()
+        {
+            ViewBag.Nickname = "Mary";
+            ViewData["nickname"] = "David"; //ViewBag屬性名不可和ViewData的Key相同，且不論大小寫
+            ViewBag.Height = 168;
+            ViewBag.Weight = 52;
+            ViewBag.Married = false;
+
+            return View();
+        }
+
+
+
+        public ActionResult PetShop()
+        {
+            ViewData["Company"] = "旺星人寵物店";
+            ViewBag.Address = "台北市信義區松山路100號";
+
+            List<string> petList = new List<string>();
+            petList.Add("狗");
+            petList.Add("貓");
+            petList.Add("魚");
+            petList.Add("鼠");
+            petList.Add("變色龍");
+
+            ViewData.Model = petList;
+
+            return View();
+        }
+
+        public ActionResult PassTempData()
+        {
+            TempData["ErrorMessage"] = "無足夠權限存取系統資料...";
+            TempData["UserName"] = "David";
+            TempData["Time"] = DateTime.Now.ToLongTimeString();
+
+            return RedirectToAction("ErrorMessage", "ErrorHandler");
+        }
+
+
+        public ActionResult PassViewModel()
+        {
+            PeopleViewModel peopleVM = new PeopleViewModel();
+            peopleVM.Employees = null;
+            peopleVM.Friends = null;
+
+            return View(peopleVM);
+        }
+    }
+}
---
+++ MvcBasic_Clone/Models/Info.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcBasic_Clone.Models
+{
+    public class Info
+    {
+        public int Id { get; set; }
+        [Display(Name = "姓名")]
+        [Required(ErrorMessage = "必須輸入姓名")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "最少需要2個字元")]
+        public string Name { get; set; }
+        [Display(Name = "手機")]
+        [RegularExpression(@"^\d{4}\-?\d{3}\-?\d{3}$", ErrorMessage = "格式需為09xx-xxx-xxx")]
+        [Required(ErrorMessage = "必須輸入手機號碼")]
+        public string Phone { get; set; }
+        [Display(Name = "Email信箱")]
+        [Required(ErrorMessage = "必須輸入Email信箱帳號")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Display(Name = "性別")]
+        [Required(ErrorMessage = "必須輸入性別")]
+        public string Gender { get; set; }
+        
+    }
+}
